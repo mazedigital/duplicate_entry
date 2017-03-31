32430912c2ce94ae828e1b8985a693167c5f2f65
Viktoryia Skrytskaya <viktoryia.skrytskaya@gmail.com>
2017-03-31 13:31:17 +0200

    move btn to the top and rename it

--- assets/duplicate_entry.js
+++ assets/duplicate_entry.js
@@ -3,15 +3,14 @@
  */
 (function ($) {
 	Symphony.Language.add({
-		"Save as New": false,
-		"or": false,
+		"Duplicate Entry": false,
 	});
+
 
 	var DuplicateEntry = {
 		
 		init: function() {
-			var actions = $('div.actions');
-			var save_button = actions.find('input[type="submit"]');
+			var save_button = $('fieldset.primary');
 			
 			var form = $('form');
 			var form_action = form.attr('action');
@@ -19,6 +18,7 @@
 			var current_section = Symphony.Context.get('duplicate-entry')['current-section'];
 			var duplicate_sections = Symphony.Context.get('duplicate-entry')['duplicate-sections'];
 			var sections = '';
+
 
 			// when duplicate_sections is not a blank array
 			if (duplicate_sections != null && duplicate_sections != 'null') {
@@ -32,27 +32,33 @@
 			}
 			
 			save_button.after('<span id="duplicate-entry" style="display:block;float:right;"></span>');
-			
 			$('#duplicate-entry')
-			.append('<span style="float:right;display:block;width:30px;text-align:center;margin-right:-10px;line-height:2;color:darkGray;">' + Symphony.Language.get("or") + '</span>')
-			.append('<input type="submit" value="' + Symphony.Language.get("Save as New") + '" id="duplicate-button" name="action[save-duplicate]"/>')
+			
+			.append('<input type="submit" value="' + Symphony.Language.get("Duplicate Entry") + '" id="duplicate-button" name="action[save-duplicate]"/>')
 			.append(sections);
 			
 			$('#duplicate-button').click(function() {
 
+
 				var clearFields = Symphony.Context.get('duplicate-entry')['clear-fields'];
+
 
 				for (var i = clearFields.length - 1; i >= 0; i--) {
 
+
 					$element = $('input[name="fields['+clearFields[i]+']"]');
+
 
 					if ($element.attr('type')=='checkbox'){
 						$element.attr('checked',false);
 					} else {
 						$element.val('');
 
+
 					}
 				}
+
+
 
 
 				$(this).attr('name', 'action[save]');
@@ -65,6 +71,7 @@
 		}
 		
 	}
+
 
 	$(function() {
 		// do not show when in Subsection Manager
